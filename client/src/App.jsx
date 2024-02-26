ace9e18e18c5263bea10f02be7fdeb3ad19bf07a
Germinal Camps <gcampsbarjau@gmail.com>
2024-02-26 12:59:46 +0100

    germinal style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,86 +9,101 @@ import Page404 from "./pages/Page404";
 function App() {
   return (
     <>
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
-        <div className="container">
-          <img src="./src/assets/BTS_Images/dognose.webp" width="30" height="24" alt="Logo" />
-          <a className="navbar-brand">Boop the Snoot</a>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
+          <div className="container">
+            <img
+              src="./src/assets/BTS_Images/dognose.webp"
+              width="30"
+              height="24"
+              alt="Logo"
+            />
+            <a className="navbar-brand">Boop the Snoot</a>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <div className="d-flex flex-column align-items-center">
-                  <img src="./src/assets/BTS_Images/doghouse.webp" width="30" height="24" alt="Icon" />
-                  <Link className="nav-link" to="/">
-                    Home
-                  </Link>
-                </div>
-              </li>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <div className="d-flex flex-column align-items-center">
+                    <img
+                      src="./src/assets/BTS_Images/doghouse.webp"
+                      width="30"
+                      height="24"
+                      alt="Icon"
+                    />
+                    <Link className="nav-link" to="/">
+                      Home
+                    </Link>
+                  </div>
+                </li>
 
-              <li className="nav-item">
-                <div className="d-flex flex-column align-items-center">
-                  <img src="./src/assets/BTS_Images/dogbook.webp" width="30" height="24" alt="Icon" />
-                  <Link className="nav-link" to="/registration">
-                    To Register
-                  </Link>
-                </div>
-              </li>
+                <li className="nav-item">
+                  <div className="d-flex flex-column align-items-center">
+                    <img
+                      src="./src/assets/BTS_Images/dogbook.webp"
+                      width="30"
+                      height="24"
+                      alt="Icon"
+                    />
+                    <Link className="nav-link" to="/registration">
+                      To Register
+                    </Link>
+                  </div>
+                </li>
 
-              <li className="nav-item">
-                <div className="d-flex flex-column align-items-center">
-                  <img src="./src/assets/BTS_Images/dogwalk.webp" width="30" height="24" alt="Icon" />
-                  <Link className="nav-link" to="/bookings">
-                    To Book
-                  </Link>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-        <div style={{ backgroundColor: "white" }}>
-        <section className="bg-white text-dark p-8 text-center">
-          <div className="container" style={{ maxWidth: "80%" }}>
-            <div className="d-sm-flex align-items-center">
-              <img
-                src="./src/assets/BTS_Images/istockphoto-814928228-2048x2048.jpg"
-                style={{ width: "50%", height: "150" }}
-                alt="Boop the Snoot"
-                className="float-left"
-              />
-              <div className="flex-grow-1 text-left pl-5">
-                <h2>text goes here</h2>
-              </div>
+                <li className="nav-item">
+                  <div className="d-flex flex-column align-items-center">
+                    <img
+                      src="./src/assets/BTS_Images/dogwalk.webp"
+                      width="30"
+                      height="24"
+                      alt="Icon"
+                    />
+                    <Link className="nav-link" to="/bookings">
+                      To Book
+                    </Link>
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
-        </section>
-      </div>
+        </nav>
 
+        <div style={{ backgroundColor: "white" }}>
+          <section className="bg-white text-dark p-8 text-center">
+            <div className="container" style={{ maxWidth: "80%" }}>
+              <div className="d-sm-flex align-items-center">
+                <img
+                  src="./src/assets/BTS_Images/istockphoto-814928228-2048x2048.jpg"
+                  style={{ width: "50%", height: "150" }}
+                  alt="Boop the Snoot"
+                  className="float-left"
+                />
+                <div className="flex-grow-1 text-left pl-5">
+                  <h2>text goes here</h2>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
 
-
-
-      <div className="row">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/registration" element={<Registration />} />
           <Route path="/bookings" element={<Bookings />} />
           <Route path="*" element={<Page404 />} />
         </Routes>
-      </div>
       </div>
     </>
   );
--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -1,259 +1,252 @@
-import React from "react";
-
 import { useEffect, useState } from "react";
-import { Container, Form, Button } from "react-bootstrap";
+// import { Container, Form, Button } from "react-bootstrap";
 
-export default function Registration() { 
+export default function Registration() {
   const [formData, setFormData] = useState({
-  dogName: "",
-  age: "",
-  breed: "",
-  size: "",
-  vaccination: "",
-  neutered: "",
-  gender: "",
-  ownerName: "",
-  email: "",
-  address: "",
-  phoneNumber: "",
-  secondContactName: "",
-  secondPhoneNumber: "",
-});
-
-const [dogs, setDogs] = useState([]);
-
-useEffect(() => {
-  getDogs();
-}, []);
-
-const getDogs = () => {
-  fetch("/api/dogs")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Failed to fetch dogs");
-    }
-    return response.json();
-  })
-  .then((dogs) => {
-    setDogs(dogs);
-  })
-  .catch((error) => {
-    console.error(error);
+    dogName: "",
+    age: "",
+    breed: "",
+    size: "",
+    vaccination: "",
+    neutered: "",
+    gender: "",
+    ownerName: "",
+    email: "",
+    address: "",
+    phoneNumber: "",
+    secondContactName: "",
+    secondPhoneNumber: "",
   });
-};
 
-async function handleChange(e) {
-  const { name, value } = e.target;
-  setFormData({
-    ...formData,
-    [name]: value,
-  });
-};
+  const [dogs, setDogs] = useState([]);
 
-async function handleSubmit(event) {
-  event.preventDefault();
-  try {
-    const response = await fetch("/api/dogs", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
+  useEffect(() => {
+    getDogs();
+  }, []);
+
+  const getDogs = () => {
+    fetch("/api/dogs")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch dogs");
+        }
+        return response.json();
+      })
+      .then((dogs) => {
+        setDogs(dogs);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  async function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
     });
-
-    if (response.ok) {
-      console.log(`"${formData.dogName}" registered successfully!`);
-    } else {
-      console.error("Failed to register dog");
-    }
-  } catch (error) {
-    console.error("Error:", error);
   }
-};
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/dogs", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log(`"${formData.dogName}" registered successfully!`);
+      } else {
+        console.error("Failed to register dog");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 
   return (
     <>
-    
-  <div className="container bg-light px-10 mb-5" >
-  <form onSubmit={handleSubmit} 
-  className="shadow p-3 mb-5 px-5 bg-white rounded">
-  
-    
+      <div className="container bg-light px-10 mb-5">
+        <form
+          onSubmit={handleSubmit}
+          className="shadow  mb-5 py-3  bg-white rounded row"
+        >
+          <label className="col-4 form-label mt-4">
+            Dogs Name
+            <input
+              className="form-control rounded"
+              type="text"
+              name="dogName"
+              value={formData.dogName}
+              onChange={handleChange}
+              placeholder="Dogs Name"
+            />
+          </label>
 
-  <label className="form-label mt-4 px-5">
-    Dogs Name
-    <input 
-    className="form-control rounded"
-    type="text"
-    name="dogName"
-    value={formData.dogName}
-    onChange={handleChange}
-    placeholder="Dogs Name"
-    /> 
-  </label>
+          <label className="col-4 form-label mt-4">
+            Dogs Age
+            <input
+              className="form-control rounded"
+              type="text"
+              name="age"
+              value={formData.age}
+              onChange={handleChange}
+              placeholder="Dogs Age"
+            />
+          </label>
 
-  <label className="form-label mt-4 px-5">
-    Dogs Age
-    <input 
-    className="form-control rounded"
-    type="text"
-    name="age"
-    value={formData.age}
-    onChange={handleChange}
-    placeholder="Dogs Age"
-    /> 
-  </label>
+          <label className="col-4 form-label mt-4">
+            Breed
+            <input
+              className="form-control rounded"
+              type="text"
+              name="breed"
+              value={formData.breed}
+              onChange={handleChange}
+              placeholder="Dogs Breed"
+            />
+          </label>
 
-  <label className="form-label mt-4 px-5">
-    Breed
-    <input 
-    className="form-control rounded"
-    type="text"
-    name="breed"
-    value={formData.breed}
-    onChange={handleChange}
-    placeholder="Dogs Breed"
-    /> 
-  </label>
+          <label className="col-4 form-label mt-4">
+            Size
+            <input
+              className="form-control rounded"
+              type="text"
+              name="size"
+              value={formData.size}
+              onChange={handleChange}
+              placeholder="Dogs Size"
+            />
+          </label>
 
-  <label className="form-label mt-4 px-5">
-    Size
-    <input 
-    className="form-control rounded"
-    type="text"
-    name="size"
-    value={formData.size}
-    onChange={handleChange}
-    placeholder="Dogs Size"
-    /> 
-  </label>
-   
-  <label className="form-label mt-4 px-5">
-    Vaccination 
-    <input 
-    className="form-control rounded"
-    type="text"
-    name="vaccination"
-    value={formData.vaccination}
-    onChange={handleChange}
-    placeholder="Dogs Vaccination"
-    /> 
-  </label>
+          <label className="col-4 form-label mt-4">
+            Vaccination
+            <input
+              className="form-control rounded"
+              type="text"
+              name="vaccination"
+              value={formData.vaccination}
+              onChange={handleChange}
+              placeholder="Dogs Vaccination"
+            />
+          </label>
 
-  <label className="form-label mt-4 px-5">
-  Neutered
-  <select
-    className="form-control rounded"
-    name="neutered"
-    value={formData.neutered}
-    onChange={handleChange}
-    >
-    <option value="">Select</option>
-    <option value="yes">Yes</option>
-    <option value="no">No</option>
-    </select>
-    </label>
+          <label className="col-4 form-label mt-4">
+            Neutered
+            <select
+              className="form-control rounded"
+              name="neutered"
+              value={formData.neutered}
+              onChange={handleChange}
+            >
+              <option value="">Select</option>
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </select>
+          </label>
 
-    <label className="form-label mt-4 px-5">
-  Gender
-  <select
-    className="form-control rounded"
-    name="gender"
-    value={formData.gender}
-    onChange={handleChange}
-    >
-    <option value="">Select</option>
-    <option value="female">Female</option>
-    <option value="male">Male</option>
-  </select>
-</label>
+          <label className="col-4 form-label mt-4">
+            Gender
+            <select
+              className="form-control rounded"
+              name="gender"
+              value={formData.gender}
+              onChange={handleChange}
+            >
+              <option value="">Select</option>
+              <option value="female">Female</option>
+              <option value="male">Male</option>
+            </select>
+          </label>
 
-<label className="form-label mt-4 px-5">
-  Owner's Name
-  <input 
-    className="form-control rounded"
-    type="text"
-    name="ownerName"
-    value={formData.ownerName}
-    onChange={handleChange}
-    placeholder="OwnersName"
-    /> 
-</label>
+          <label className="col-4 form-label mt-4">
+            Owner's Name
+            <input
+              className="form-control rounded"
+              type="text"
+              name="ownerName"
+              value={formData.ownerName}
+              onChange={handleChange}
+              placeholder="OwnersName"
+            />
+          </label>
 
-<label className="form-label mt-4 px-5">
-  email
-  <input 
-    className="form-control rounded"
-    type="text"
-    name="email"
-    value={formData.email}
-    onChange={handleChange}
-    placeholder="email"
-    /> 
-</label>
+          <label className="col-4 form-label mt-4">
+            email
+            <input
+              className="form-control rounded"
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="email"
+            />
+          </label>
 
-<label className="form-label mt-4 px-5">
-  Address
-  <input 
-    className="form-control rounded"
-    type="text"
-    name="address"
-    value={formData.address}
-    onChange={handleChange}
-    placeholder="address"
-    /> 
-</label>
+          <label className="col-4 form-label mt-4">
+            Address
+            <input
+              className="form-control rounded"
+              type="text"
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+              placeholder="address"
+            />
+          </label>
 
-<label className="form-label mt-4 px-5">
-    Phone Number
-  <input 
-    className="form-control rounded"
-    type="text"
-    name="phoneNumber"
-    value={formData.phoneNumber}
-    onChange={handleChange}
-    placeholder="PhoneNumber"
-    /> 
-</label>
+          <label className="col-4 form-label mt-4">
+            Phone Number
+            <input
+              className="form-control rounded"
+              type="text"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              placeholder="PhoneNumber"
+            />
+          </label>
 
-<label className="form-label mt-4 px-5">
-  Secondary Contact Name
-  <input 
-   className="form-control rounded"
-   type="text"
-   name="secondContactName"
-   value={formData.secondContactName}
-   onChange={handleChange}
-   placeholder="ContactName"
-    /> 
-</label>
+          <label className="col-4 form-label mt-4">
+            Secondary Contact Name
+            <input
+              className="form-control rounded"
+              type="text"
+              name="secondContactName"
+              value={formData.secondContactName}
+              onChange={handleChange}
+              placeholder="ContactName"
+            />
+          </label>
 
-<label className="form-label mt-4 px-5">
-  Secondary Phone Number
-  <input 
-    className="form-control rounded"
-    type="text"
-    name="secondPhoneNumber"
-    value={formData.secondPhoneNumber}
-    onChange={handleChange}
-    placeholder="PhoneNumber"
-    /> 
-</label>
+          <label className="col-4 form-label mt-4">
+            Secondary Phone Number
+            <input
+              className="form-control rounded"
+              type="text"
+              name="secondPhoneNumber"
+              value={formData.secondPhoneNumber}
+              onChange={handleChange}
+              placeholder="PhoneNumber"
+            />
+          </label>
 
-
-<button type="submit"
-   className="btn btn-primary">
- <i className="fa-solid fa-paw"></i><br/>
-  Submit
-  </button>
-  </form>
-  </div>
-  
-  </>
+          <div className="col-4 mt-4">
+            <button type="submit" className="btn btn-primary">
+              <i className="fa-solid fa-paw"></i>
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
-
 
 // 1. Do I need new state (data) for this? (Give it a name)
 // 2. What is the initial value for that data
